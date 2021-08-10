79ddb1307b534eb1b7a5d63ef0a4809a0799ed3a
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2021-08-10 21:36:32 +0545

    feat: add base

---
+++ lib/snowflakeapirb.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "snowflakeapirb/version"
+
+module Snowflakeapirb
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/snowflakeapirb/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Snowflakeapirb
+  VERSION = "0.1.0"
+end
